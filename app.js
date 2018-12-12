82c3ee090b593994ef91c82765103425aa8693a5
Aruna Liyanaarachchi <liyanaarachchi@Arunas-MBP.fios-router.home>
2018-12-12 10:15:31 -0500

    Output Update

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');				//npm install express
 const bodyParser = require('body-parser');		//npm install body-parser
 const BrewNode = require('./brewNode'); 		//local class
+var rp = require('request-promise');
+
+
+
+
+var options = {
+    method: 'POST',
+    uri: 'http://localhost:3009/spawnBrew/D',
+    body: {
+        some: 'payload'
+    },
+    json: true // Automatically stringifies the body to JSON
+};
+
+
 
 //
 const port = 18070+Math.floor(Math.random()*30);
@@ -33,6 +48,17 @@ let BrewHTTP = function (){
 		
 		//////////////////////////////////////////////////////////////
 	})
+	
+
+	app.get('/spawn/D', function (req, res) {
+        rp(options)
+        .then(function (parsedBody) {
+            res.send(parsedBody)
+        })
+        .catch(function (err) {
+            res.send(err)
+        });
+		});
 
 	app.listen(http_port, () => {
 		console.log(`http server up.. ${http_port}`);
